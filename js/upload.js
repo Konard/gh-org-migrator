968f9dd54a49e608e0757da30d77c558cac94bba
Konstantin Diachenko <drakonard@gmail.com>
2024-07-29 23:38:08 +0300

    Make a check for existing issues better

--- js/upload.js
+++ js/upload.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
 import { Octokit } from "@octokit/rest";
-import fs from 'fs';
-import path from 'path';
-import { config } from 'dotenv';
-import simpleGit from 'simple-git';
-import inquirer from 'inquirer';
+import fs from "fs";
+import path from "path";
+import { config } from "dotenv";
+import simpleGit from "simple-git";
+import inquirer from "inquirer";
+import { createCache, memoryStore } from "cache-manager";
+
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+const cache = createCache(
+  memoryStore({
+    max: 100,
+    ttl: hour,
+  }),
+);
 
 // Load environment variables
 config();
@@ -13,29 +24,32 @@ config();
 const { ACCESS_TOKEN, SOURCE_ORGANIZATION, TARGET_ORGANIZATION } = process.env;
 
 if (!ACCESS_TOKEN || !SOURCE_ORGANIZATION || !TARGET_ORGANIZATION) {
-  console.error("ACCESS_TOKEN, SOURCE_ORGANIZATION, and TARGET_ORGANIZATION must be set in the .env file.");
+  console.error(
+    "ACCESS_TOKEN, SOURCE_ORGANIZATION, and TARGET_ORGANIZATION must be set in the .env file.",
+  );
   process.exit(1);
 }
 
 // Initialize Octokit
 const octokit = new Octokit({
-  auth: ACCESS_TOKEN
+  auth: ACCESS_TOKEN,
 });
 
 // Initialize simple-git
 const git = simpleGit();
 
 // Path to the input data directory
-const INPUT_DIR = path.join(process.cwd(), 'data', SOURCE_ORGANIZATION);
+const INPUT_DIR = path.join(process.cwd(), "data", SOURCE_ORGANIZATION);
 
 const defaultIntervalMs = 30000;
 
-const sleep = async (ms) => await new Promise(resolve => setTimeout(resolve, ms));
+const sleep = async (ms) =>
+  await new Promise((resolve) => setTimeout(resolve, ms));
 
 // Utility function to read JSON data from a file
 function readJSONFromFile(filePath) {
   if (fs.existsSync(filePath)) {
-    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    return JSON.parse(fs.readFileSync(filePath, "utf-8"));
   } else {
     console.error(`File not found: ${filePath}`);
     process.exit(1);
@@ -47,7 +61,7 @@ async function repositoryExists(repoName) {
   try {
     await octokit.repos.get({
       owner: TARGET_ORGANIZATION,
-      repo: repoName
+      repo: repoName,
     });
     return true;
   } catch (error) {
@@ -63,7 +77,9 @@ async function repositoryExists(repoName) {
 // Create a repository in the target organization
 async function createRepository(repo) {
   try {
-    console.log(`Creating repository ${repo.name} in organization ${TARGET_ORGANIZATION}...`);
+    console.log(
+      `Creating repository ${repo.name} in organization ${TARGET_ORGANIZATION}...`,
+    );
     const response = await octokit.repos.createInOrg({
       org: TARGET_ORGANIZATION,
       name: repo.name,
@@ -83,16 +99,61 @@ async function createRepository(repo) {
   }
 }
 
+async function fetchAllIssues(repoName, page = 1, issues = []) {
+  // console.log({ fetchAllIssues: { repoName } });
+  const { data: fetchedIssues } = await octokit.issues.listForRepo({
+    owner: TARGET_ORGANIZATION,
+    repo: repoName,
+    state: "open",
+    per_page: 100,
+    page,
+  });
+
+  issues = issues.concat(fetchedIssues);
+
+  if (fetchedIssues.length === 100) {
+    // If the current page is full, there might be more issues, so fetch the next page
+    return await fetchAllIssues(repoName, page + 1, issues);
+  } else {
+    // All issues fetched
+    return issues;
+  }
+}
+
+async function getCachedIssues(repoName) {
+  try {
+    // console.log({ getCachedIssues: { repoName } });
+
+    const key = `${repoName}-issues`;
+    // Check if issues are already cached
+    const cachedIssues = await cache.get(key);
+    if (cachedIssues) {
+      // console.log("Returning cached issues");
+      return cachedIssues;
+    }
+
+    // If not cached, fetch all issues from GitHub
+    const issues = await fetchAllIssues(repoName);
+
+    // Cache the fetched issues
+    await cache.set(key, issues);
+    // console.log("Fetched and cached issues from GitHub");
+    return issues;
+  } catch (error) {
+    console.error("Error fetching issues from GitHub:", error);
+    throw error;
+  }
+}
+
 // Check if an issue exists in a repository
 async function issueExists(repoName, issue) {
   try {
-    const { data: issues } = await octokit.issues.listForRepo({
-      owner: TARGET_ORGANIZATION,
-      repo: repoName
-    });
-    return issues.some(i => i.title === issue.title && i.body === issue.body);
+    const issues = await getCachedIssues(repoName);
+    return issues.some((i) => i.title === issue.title && i.body === issue.body);
   } catch (error) {
-    console.error(`Error checking issue "${issue.title}" in repository ${repoName}: ${error.message}`);
+    console.error(
+      `Error checking issue "${issue.title}" in repository ${repoName}: ${error.message}`,
+    );
     process.exit(1);
   }
 }
@@ -102,7 +163,9 @@ async function checkRateLimit() {
   try {
     const response = await octokit.rateLimit.get();
     const rateLimit = response.data.resources.core;
-    console.log(`Rate limit: ${rateLimit.remaining}/${rateLimit.limit}, resets at ${new Date(rateLimit.reset * 1000)}`);
+    console.log(
+      `Rate limit: ${rateLimit.remaining}/${rateLimit.limit}, resets at ${new Date(rateLimit.reset * 1000)}`,
+    );
     return rateLimit.remaining;
   } catch (error) {
     console.error(`Error checking rate limit: ${error.message}`);
@@ -118,65 +181,62 @@ async function createIssues(repoName, issues) {
       try {
         const remaining = await checkRateLimit();
         if (remaining < 10) {
-          const waitTime = (new Date(response.headers['x-ratelimit-reset'] * 1000).getTime() - new Date().getTime()) + 10000;
-          console.log(`Low rate limit remaining (${remaining}). Waiting until rate limit resets...`);
+          const waitTime =
+            new Date(response.headers["x-ratelimit-reset"] * 1000).getTime() -
+            new Date().getTime() +
+            10000;
+          console.log(
+            `Low rate limit remaining (${remaining}). Waiting until rate limit resets...`,
+          );
           await sleep(waitTime);
         }
 
-        console.log(`Creating issue "${issue.title}" in repository ${repoName}...`);
+        console.log(
+          `Creating issue "${issue.title}" in repository ${repoName}...`,
+        );
         await octokit.issues.create({
           owner: TARGET_ORGANIZATION,
           repo: repoName,
           title: issue.title,
-          body: issue.body
+          body: issue.body,
         });
         await sleep(defaultIntervalMs);
       } catch (error) {
-        console.error(`Error creating issue "${issue.title}" in repository ${repoName}: ${error.message}`);
+        console.error(
+          `Error creating issue "${issue.title}" in repository ${repoName}: ${error.message}`,
+        );
         process.exit(1);
       }
     } else {
-      console.log(`Issue "${issue.title}" already exists in repository ${repoName}. Skipping creation.`);
+      console.log(
+        `Issue "${issue.title}" already exists in repository ${repoName}. Skipping creation.`,
+      );
     }
-  }
-}
-
-async function getAllRemoteBranches() {
-  try {
-    const remoteBranches = await git.listRemote(['--heads', 'origin']);
-    const branches = remoteBranches
-      .split('\n')
-      .map(line => line.split('\t')[1])
-      .filter(Boolean)
-      .map(branch => branch.replace('refs/heads/', ''));
-
-    console.log('Remote branches:', branches);
-    return branches;
-  } catch (err) {
-    console.error('Error getting remote branches:', err);
   }
 }
 
 // Fetch all branches
 async function fetchAllBranches(repoDir) {
-  return await git.cwd(repoDir).fetch(['--all']);
+  return await git.cwd(repoDir).fetch(["--all"]);
   // await git.cwd(repoDir).fetch('origin');
 }
 
 // List remote branches and create local tracking branches
 async function createLocalTrackingBranches(repoDir) {
-  const remoteBranches = await git.cwd(repoDir).branch(['-r']);
+  const remoteBranches = await git.cwd(repoDir).branch(["-r"]);
   const localBranches = await git.cwd(repoDir).branch();
 
   for (const remoteBranch of remoteBranches.all) {
     // console.log({ remoteBranch });
-    if (!remoteBranch.includes('->')) {
-      const branchName = remoteBranch.replace('origin/', '');
+    if (!remoteBranch.includes("->")) {
+      const branchName = remoteBranch.replace("origin/", "");
       if (!localBranches.all.includes(branchName)) {
-        await git.cwd(repoDir).branch(['--track', branchName, remoteBranch]);
+        await git.cwd(repoDir).branch(["--track", branchName, remoteBranch]);
         console.log(`Created local tracking branch: ${branchName}`);
       } else {
-        await git.cwd(repoDir).branch(['--set-upstream-to', `origin/${branchName}`, branchName]);
+        await git
+          .cwd(repoDir)
+          .branch(["--set-upstream-to", `origin/${branchName}`, branchName]);
         console.log(`Updated upstream for branch: ${branchName}`);
       }
     }
@@ -189,10 +249,14 @@ async function pullAllLocalBranches(repoDir) {
   for (const branch of localBranches.all) {
     await git.cwd(repoDir).checkout(branch);
     try {
-      await git.cwd(repoDir).pull({ "--ff-only": null, '--strategy-option': 'theirs' });
+      await git
+        .cwd(repoDir)
+        .pull({ "--ff-only": null, "--strategy-option": "theirs" });
       console.log(`Pulled latest changes for branch: ${branch}`);
     } catch (error) {
-      console.error(`Error pulling latest changes for branch: ${branch}: ${error.message}`);
+      console.error(
+        `Error pulling latest changes for branch: ${branch}: ${error.message}`,
+      );
     }
   }
 }
@@ -208,8 +272,12 @@ async function pushAllLocalBranches(repoDir) {
     let tryAgain = false;
     do {
       try {
-        const pushedBranches = await git.cwd(repoDir).push('origin', branchName);
-        const currentBranchAlreadyUpdated = pushedBranches?.pushed?.length <= 0 || pushedBranches.pushed.every(i => i.alreadyUpdated);
+        const pushedBranches = await git
+          .cwd(repoDir)
+          .push("origin", branchName);
+        const currentBranchAlreadyUpdated =
+          pushedBranches?.pushed?.length <= 0 ||
+          pushedBranches.pushed.every((i) => i.alreadyUpdated);
         if (!currentBranchAlreadyUpdated) {
           branchesAlreadyUpdated = false;
         }
@@ -220,14 +288,16 @@ async function pushAllLocalBranches(repoDir) {
         }
         tryAgain = false;
       } catch (error) {
-        console.error(`Error pulling latest changes for branch: ${branchName}: ${error.message}`);
+        console.error(
+          `Error pulling latest changes for branch: ${branchName}: ${error.message}`,
+        );
         const { confirm } = await inquirer.prompt([
           {
-            type: 'confirm',
-            name: 'confirm',
+            type: "confirm",
+            name: "confirm",
             message: `Do you want to try push latest changes to ${branchName} branch again?`,
-            default: false
-          }
+            default: false,
+          },
         ]);
         tryAgain = confirm;
         if (!tryAgain) {
@@ -247,10 +317,17 @@ async function pushRepository(repoName) {
     process.exit(1);
   }
   try {
-    console.log(`Pushing repository ${repoName} to organization ${TARGET_ORGANIZATION}...`);
+    console.log(
+      `Pushing repository ${repoName} to organization ${TARGET_ORGANIZATION}...`,
+    );
 
-    await git.cwd(repoDir).removeRemote('origin');
-    await git.cwd(repoDir).addRemote('origin', `https://github.com/${SOURCE_ORGANIZATION}/${repoName}.git`);
+    await git.cwd(repoDir).removeRemote("origin");
+    await git
+      .cwd(repoDir)
+      .addRemote(
+        "origin",
+        `https://github.com/${SOURCE_ORGANIZATION}/${repoName}.git`,
+      );
 
     await fetchAllBranches(repoDir);
 
@@ -258,14 +335,23 @@ async function pushRepository(repoName) {
 
     await pullAllLocalBranches(repoDir);
 
-    await git.cwd(repoDir).removeRemote('origin');
-    await git.cwd(repoDir).addRemote('origin', `https://github.com/${TARGET_ORGANIZATION}/${repoName}.git`);
+    await git.cwd(repoDir).removeRemote("origin");
+    await git
+      .cwd(repoDir)
+      .addRemote(
+        "origin",
+        `https://github.com/${TARGET_ORGANIZATION}/${repoName}.git`,
+      );
 
     const branchesAlreadyUpdated = await pushAllLocalBranches(repoDir);
 
-    const pushedTags = await git.cwd(repoDir).pushTags('origin');
+    const pushedTags = await git.cwd(repoDir).pushTags("origin");
 
-    if (branchesAlreadyUpdated && (pushedTags?.pushed?.length <= 0 || pushedTags.pushed.every(i => i.alreadyUpdated))) {
+    if (
+      branchesAlreadyUpdated &&
+      (pushedTags?.pushed?.length <= 0 ||
+        pushedTags.pushed.every((i) => i.alreadyUpdated))
+    ) {
       console.log(`Repository ${repoName} is already up to date.`);
     } else {
       console.log(`Repository ${repoName} pushed successfully.`);
@@ -279,35 +365,40 @@ async function pushRepository(repoName) {
 
 async function main() {
   try {
-    const repoFilePath = path.join(INPUT_DIR, 'org.repos.json');
+    const repoFilePath = path.join(INPUT_DIR, "org.repos.json");
     const repos = readJSONFromFile(repoFilePath);
 
-    // Repos
-    for (const repo of repos) {
-      const repoName = repo.name;
-      const exists = await repositoryExists(repoName);
-      if (!exists) {
-        await createRepository(repo);
-      } else {
-        console.log(`Repository ${repoName} already exists. Skipping creation.`);
-      }
-    }
+    // // Repos
+    // for (const repo of repos) {
+    //   const repoName = repo.name;
+    //   const exists = await repositoryExists(repoName);
+    //   if (!exists) {
+    //     await createRepository(repo);
+    //   } else {
+    //     console.log(
+    //       `Repository ${repoName} already exists. Skipping creation.`,
+    //     );
+    //   }
+    // }
 
-    // Code commits
-    for (const repo of repos) {
-      const repoName = repo.name;
-      await pushRepository(repoName);
-    }
+    // // Code commits
+    // for (const repo of repos) {
+    //   const repoName = repo.name;
+    //   await pushRepository(repoName);
+    // }
 
     // Issues
     for (const repo of repos) {
       const repoName = repo.name;
+      // console.log({ repoName });
       const issuesFilePath = path.join(INPUT_DIR, `${repoName}.issues.json`);
       const issues = readJSONFromFile(issuesFilePath);
       await createIssues(repoName, issues);
     }
 
-    console.log(`Data uploading completed. All data is uploaded to the ${TARGET_ORGANIZATION} organization.`);
+    console.log(
+      `Data uploading completed. All data is uploaded to the ${TARGET_ORGANIZATION} organization.`,
+    );
   } catch (error) {
     console.error(`Unexpected error: ${error.message}`);
     process.exit(1);
